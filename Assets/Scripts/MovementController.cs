7dc4d1d3f6f1ad5efe7c94e1475524400def85c7
macadamc <macadamchris@gmail.com>
2016-06-27 17:49:01 -0400

    camredo

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+    public float moveSpd = 5f;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void Move(Vector2 moveVector)
+    {
+        rb.velocity += moveVector * moveSpd * Time.deltaTime;
+
+        if (rb.velocity.magnitude > moveSpd)
+            rb.velocity = rb.velocity.normalized * moveSpd;
+    }
+}
---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MovementController))]
+public class PlayerInputController : MonoBehaviour {
+
+    Vector2 axisInput;
+    bool fire1;
+    bool fire2;
+
+    MovementController mc;
+
+	// Use this for initialization
+	void Awake () {
+
+        mc = GetComponent<MovementController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        axisInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        fire1 = Input.GetButton("Fire1");
+        fire2 = Input.GetButton("Fire2");
+
+        mc.Move(axisInput);
+
+    }
+}
